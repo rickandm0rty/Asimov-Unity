e13ce6e7c76c5d1f7bcd7bdd8e898fb93d088773
Kyle Nelson <kylenelson272@u.boisestate.edu>
2018-09-16 08:17:05 -0600

    Shooting Mechanics, Particle System
    Added a basic left click fire with a particle. Particle doesn't currently show up but its point light does. Needs tweaking.

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -5,19 +5,31 @@ public class Gun : MonoBehaviour {
 	public float damage = 10f;
 	public float range = 100f;
 	public Camera fpsCam;
-	public ParticleSystem muzzleFlash;
+	public GameObject bulletPrefab;
+	//public Transform bulletSpawn;
+	//public ParticleSystem muzzleFlash;
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown("Fire1"))
 		{
+			//Debug.Log("Space Key Hit");
 			Shoot();		
 		}
 	}
 
 	void Shoot()
 	{
-		muzzleFlash.Play();
+	//muzzleFlash.Play();
+
+	// Create the Bullet from the Bullet Prefab
+    	var bullet = (GameObject)Instantiate (bulletPrefab, transform.position,transform.rotation);
+
+    // Add velocity to the bullet
+    	bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 12;
+
+    // Destroy the bullet after 2 seconds
+    	Destroy(bullet, 2.0f);
 		
 		RaycastHit hit;
 		if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
